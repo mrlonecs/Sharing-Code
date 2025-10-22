8fb99959aedba7c9a7d74f9a072c5127ce04f236
mrlonecs <kslwhitgift@gmail.com>
2025-10-22 17:16:05 +0100

    Create examMark.py

---
+++ examMark.py
@@ -0,0 +1,11 @@
+mark = int(input("Enter the exam mark: "))
+if mark > 80:
+    print("A*")
+if mark > 75:
+    print("A")
+if mark > 60:
+    print("B")
+if mark > 50:
+    print("C")
+else:
+    print("D")
